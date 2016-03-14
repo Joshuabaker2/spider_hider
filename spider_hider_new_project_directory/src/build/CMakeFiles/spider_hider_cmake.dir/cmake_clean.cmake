8966720111e0314a94d78476888ab66b3c9a9353
Joshua Baker <joshuabaker2@gmail.com>
2016-03-13 18:19:27 -0700

    Initial spider hider that compiles to Javascript using emscripten

---
+++ spider_hider_new_project_directory/src/build/CMakeFiles/spider_hider_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spider_hider_cmake.dir/spider_hider.cpp.o"
+  "spider_hider_cmake.pdb"
+  "spider_hider_cmake.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spider_hider_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ spider_hider_new_project_directory/src/spider_hider.cpp
@@ -0,0 +1,54 @@
+#define DLIB_NO_GUI_SUPPORT
+// #define DLIB_JPEG_SUPPORT
+#define DLIB_PNG_SUPPORT
+#define NO_MAKEFILE
+
+#include <dlib/svm_threaded.h>
+#include <dlib/image_processing.h>
+#include <dlib/data_io.h>
+
+#include <stdio.h>
+#include <emscripten.h>
+
+using namespace std;
+using namespace dlib;
+
+// ----------------------------------------------------------------------------------------
+// g++ -o spider_hider -O3 -I.. spider_hider.cpp -lpng
+// emcc -I.. -s USE_LIBPNG=1 spider_hider.cpp
+// emcc -O3 -I.. -s USE_LIBPNG=1 spider_hider.cpp -o spider_hider.js
+int main(int argc, char** argv) {
+    try {
+
+        // mount the current folder as a NODEFS instance
+        // inside of emscripten
+        EM_ASM(
+          FS.mkdir('/working');
+          FS.mount(NODEFS, { root: '.' }, '/working');
+        );
+
+        array2d<rgb_pixel> img;
+        // load the image as an array2d (dlib specific type)
+        load_image(img, "/working/brown_widow_spider_large.png");
+
+        typedef scan_fhog_pyramid<pyramid_down<10> > image_scanner_type;
+
+        object_detector<image_scanner_type> detector;
+
+        // deserialize our saved spider detector
+        deserialize("/working/spider_detector.svm") >> detector;
+
+        // Run the detector and get the spider detections.
+        std::vector<rectangle> dets = detector(img);
+        if (dets.size() > 0) {
+          printf("Spider detected");
+        } else {
+          printf("Spider not detected");
+        }
+    }
+    catch (exception& e)
+    {
+        cout << "\nexception thrown!" << endl;
+        cout << e.what() << endl;
+    }
+}
